cd58d25826f0ef063caf8e94db478ff12185e1b0
Nick <rikzgt1@gmail.com>
2017-10-10 17:34:29 +0300

    Тест отправки изменений в репозиторий 2

--- app/Http/Controllers/GtaVController.php
+++ app/Http/Controllers/GtaVController.php
@@ -12,12 +12,12 @@ class GtaVController extends Controller
         return view('gtav_info')->with('title',$title);
     }
 
-    //gtav_city.blade
+        //gtav_city.blade
     public function gtav_city() {
         $title = 'GTA V город';
         return view('gtav_city')->with('title',$title);
     }
-
+        //gtav_sist.blade
     public function gtav_sist() {
         $title = 'GTA V системные требования';
         return view('gtav_sist')->with('title',$title);
