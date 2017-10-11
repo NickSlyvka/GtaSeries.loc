56c64e22e22fb0c8e495703f156d5b9b2fc0ce35
Rikz <rikzgt1@gmail.com>
2017-10-11 14:58:31 +0300

    Измененеия 5

--- app/Http/Controllers/GtaVController.php
+++ app/Http/Controllers/GtaVController.php
@@ -6,13 +6,13 @@ use Illuminate\Http\Request;
 
 class GtaVController extends Controller
 {
-        //gtav_info.blade.php
+        //gtav_info.blade
     public function gtav_info() {
         $title = 'GTA V информация';
         return view('gtav_info')->with('title',$title);
     }
 
-        //gtav_city.blade.php
+        //gtav_city.blade
     public function gtav_city() {
         $title = 'GTA V город';
         return view('gtav_city')->with('title',$title);
