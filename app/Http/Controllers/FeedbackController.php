b5c9c103a0101c9a3184cba936d43cc65048b5d4
Rikz <rikzgt1@gmail.com>
2017-10-11 16:29:59 +0300

    'third'

--- app/Http/Controllers/FeedbackController.php
+++ app/Http/Controllers/FeedbackController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 
 class FeedbackController extends Controller
 {
+        //feefback.blade.php
     public function feedback() {
         $title = 'Обратная связь';
         return view('feedback')->with('title',$title);
